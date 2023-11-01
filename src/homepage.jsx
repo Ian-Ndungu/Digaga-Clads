74591d431afc06afa9795007a850706accd45265
levina23 <levinanjambi22@gmail.com>
2023-11-01 22:10:55 +0300

    worked on cart

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cart from './cart'
 function home() {
   return (
     <div>
-        
+        <Router>
+          <Cart/>
+        </Router>
     </div>
   )
 }
