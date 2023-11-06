7139980e6a46e9a66ff56cb11951644844b7ad36
Samolivyu <Samolivyumanki@outlook.com>
2023-11-06 09:49:40 +0300

    ADDED APP JSX MODDED FILE

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react' 
-import Men from './men'
-// import button from './men.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Men from './men';
+import menData from './menData'
 
-function App() 
-{
+
+function App() {
   return (
-  <div>
-    < Men/> 
-  </div>
-  )
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/Men/Official">Official</Link>
+          </li>
+          <li>
+            <Link to="/Men/Casual">Casual</Link>
+          </li>
+          <li>
+            <Link to="/Men/Smart Casual">Smart Casual</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/Men/:category">
+            <Men menData={menData} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
