2cc9dc8c7f469ce5d42c07156f977bfc0e2dc5c6
Samolivyu <Samolivyumanki@outlook.com>
2023-11-06 09:51:06 +0300

    UODATED APP JSX FILE

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react' 
-import Women from './women'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Women from './women';
+import womenData from './womenData'
 
-function App() 
-{
+
+function App() {
   return (
-  <div>
-    < Women/> 
-  </div>
-  )
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/Women/Official">Official</Link>
+          </li>
+          <li>
+            <Link to="/Women/Casual">Casual</Link>
+          </li>
+          <li>
+            <Link to="/Women/Smart Casual">Smart Casual</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/Women/:category">
+            <Women womenData={womenData} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
