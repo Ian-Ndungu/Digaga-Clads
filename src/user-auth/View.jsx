4e9630940cc0f81bc3af2429f42c5d10104eaeaf
Akm129 <anitajehu13@gmail.com>
2023-11-07 12:40:44 +0300

    added registration view component

--- src/user-auth/View.jsx
+++ src/user-auth/View.jsx
@@ -10,6 +10,8 @@ const View = () => {
   // State to track user login error message
   const [adminLoginError, setAdminLoginError] = useState(""); 
   // State to track admin login error message
+  const [registeredUserName, setRegisteredUserName] = useState(null);
+  // State to store the registered user's name
 
   // State to track if an admin is logged in
   const [isAdminLoggedIn, setAdminLoggedIn] = useState(false);
@@ -19,9 +21,10 @@ const View = () => {
       const response = await axios.post("http://localhost:8000/admin/login/", adminCredentials);
       if (response.data.success) {
         setAdminLoggedIn(true);
-        // If admin is logged in, you can redirect or render admin-specific content
-        // For example, you can redirect to an admin dashboard
-        // window.location.href = "/admin-dashboard";
+        // Display a welcome message with the admin's name
+        window.alert(`Welcome ${response.data.adminName}!`);
+        // Redirect to the homepage
+        window.location.href = "/homepage";
       } else {
         // Handle admin login failure and display an error message
         setAdminLoggedIn(false);
@@ -32,7 +35,7 @@ const View = () => {
       setAdminLoggedIn(false);
       setAdminLoginError("An error occurred during admin login. Please try again.");
     }
-  };
+  };  
 
   const handleUserLogin = async (userCredentials) => {
     try {
@@ -40,6 +43,7 @@ const View = () => {
       if (response.data.success) {
         // Redirect users to the homepage after a successful login
         window.location.href = "/homepage";
+        window.alert("Successfully logged in!");
       } else {
         // Handle user login failure and display an error message
         setUserLoginError("User login failed. Please check your credentials.");
@@ -50,12 +54,28 @@ const View = () => {
     }
   };
 
+  const handleUserRegistration = async (userRegistrationData) => {
+    try {
+      // Assuming your registration API returns the user's name
+      const response = await axios.post("http://localhost:8000/register/", userRegistrationData);
+      if (response.data.success) {
+        setRegisteredUserName(response.data.userName);
+        window.alert(`Successfully registered as ${response.data.userName}!`);
+      } else {
+        setUserLoginError("User registration failed. Please check your data.");
+      }
+    } catch (error) {
+      console.error(error);
+      setUserLoginError("An error occurred during user registration. Please try again.");
+    }
+  };
+
   return (
     <div className="container">
       {currentView === "login" ? (
         <Login onLogin={handleUserLogin} />
       ) : currentView === "registration" ? (
-        <Register />
+        <Register onRegister={handleUserRegistration} />
       ) : (
         <Admin onAdminLogin={handleAdminLogin} />
       )}
