5a83d4660693589a533f8a3a8e776d232d93c193
Samolivyu <Samolivyumanki@outlook.com>
2023-11-07 23:39:06 +0300

    implementing the changes on the other branches.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Kids from './Kids';
-import kidsData from './kidsData'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Kids from './kids';
 
 function App() {
+
+  const [kidsData, setKidsData] = useState([]);
+  const { category } = useParams();
+
+  useEffect(() => {
+    if (category) {
+      const apiUrl = `digaga-clads-url/${category}`;
+      
+      fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setKidsData(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching kids data:', error);
+        });
+    }
+  }, [category]);
+
   return (
+
     <Router>
       <div>
-        {/* <ul>
-          <li>
-            <Link to="/kids/Official">Official</Link>
-          </li>
-          <li>
-            <Link to="/kids/Casual">Casual</Link>
-          </li>
-          <li>
-            <Link to="/kids/Smart Casual">Smart Casual</Link>
-          </li>
-        </ul> */}
+        <Switch>
           <Route path="/kids/:category">
             <Kids kidsData={kidsData} />
           </Route>
-        
+        </Switch>
       </div>
     </Router>
   );
--- src/women.jsx
+++ src/women.jsx
@@ -1,127 +1,75 @@
 import React, { useState } from 'react';
-import './women.css';
-import SelectedImage from './kidsData';
+import { useParams } from 'react-router-dom';
+import  './women.css'
 
-const Women = () => {
+const Women = ({womenData}) => 
+{
+  const { category } = useParams();
   const [selectedCategory, setSelectedCategory] = useState('All');
-  const [selectedClothing, setSelectedClothing] = useState('All');
-  const [selectedColor, setSelectedColor] = useState('All');
-  const [selectedSize, setSelectedSize] = useState('All');
+  const filteredData = womenData.filter((item) => {
+  (selectedCategory === 'All' || item.name === selectedCategory).map(
+  (item) => item.details)
+  .flat();
+  });
+  //All is to reset the selection to default for the user.
 
-  const womenData = [
-    {
-      id: 1,
-      name: 'Suits',
-      category: 'Official',
-      details: [
-        { id: 1, color: 'Black', size: 'M' },
-        { id: 2, color: 'Navy', size: 'L' },
-        { id: 3, color: 'Gray', size: 'XL' },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Sweaters',
-      category: 'Casual',
-      details: [
-        { id: 4, color: 'Red', size: 'S' },
-        { id: 5, color: 'Blue', size: 'M' },
-      ],
-    },
-    {
-      id: 3,
-      name: 'Hoodies',
-      category: 'Smart Casual',
-      details: [
-        { id: 6, color: 'Black', size: 'L' },
-        { id: 7, color: 'Gray', size: 'XL' },
-      ],
-    },
-  ];
-
-  const getAvailableClothing = () => {
-    if (selectedCategory === 'All') {
-      return womenData;
+  const fetchData = async () => {
+    try {
+      const response = await fetch('Digaga-clads-url');
+      if (!response.ok) {
+        throw new Error('Network error!');
+      }
+      const data = await response.json();
+      setWomenData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
-    return womenData.filter((item) => item.category === selectedCategory);
   };
 
-  const getAvailableColor = () => {
-    if (selectedClothing === 'All') {
-      return getAvailableClothing()
-        .flatMap((item) => item.details.map((detail) => detail.color));
-    }
-    return getAvailableClothing()
-      .find((item) => item.name === selectedClothing)
-      .details.map((detail) => detail.color);
-  };
+  useEffect(() => {
+    fetchData();
+  }, []); //Empty to feetch on completion.
 
-  const getAvailableSizes = () => {
-    if (selectedColor === 'All') {
-      return getAvailableClothing()
-        .flatMap((item) => item.details.map((detail) => detail.size))
-        .filter((value, index, self) => self.indexOf(value) === index);
-    }
-    return getAvailableClothing()
-      .find((item) => item.name === selectedClothing)
-      .details.filter((detail) => detail.color === selectedColor)
-      .map((detail) => detail.size);
-  };
-
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category);
-    setSelectedClothing('All');
-    setSelectedColor('All');
-    setSelectedSize('All');
-  };
-
-  const handleClothingChange = (clothing) => {
-    setSelectedClothing(clothing);
-    setSelectedColor('All');
-    setSelectedSize('All');
-  };
-
-  const handleColorChange = (color) => {
-    setSelectedColor(color);
-  };
-
-  const handleSizeChange = (size) => {
-    setSelectedSize(size);
-  };
+  useEffect(() => {
+    // selectedCategory filter
+    const newFilteredData = womenData.filter((item) => {
+      return selectedCategory === 'All' || item.name === selectedCategory;
+    });
+    setFilteredData(newFilteredData);
+  }, [selectedCategory, womenData]);
+// API fetch function.
 
   return (
-    <div className="landingbg">
-      <div>
-        <h1>Women's Page</h1>
-
-        <div className="categories-section">
-          <h2>Categories: </h2>
-          <ul>
-            {['Official', 'Smart Casual', 'Casual'].map((item) => (
-              <li key={item.id}>
-                <div className="category-alignment">
-                  <SelectedImage
-                    imageUrl={{
-                      normal: `/home/nesh/Digaga-Clads/public/womenlanding.jpeg`,
-                      hover: `/home/nesh/Digaga-Clads/public/womenlanding.jpeg`,
-                    }}
-                    altText={`${item.category} category`}
-                    category={item.name}
-                    availableClothing={getAvailableClothing(item.category)}
-                    onCategoryChange={handleCategoryChange}
-                    onClothingChange={handleClothingChange}
-                    onColorChange={handleColorChange}
-                    onSizeChange={handleSizeChange}
-                  />
-                  <p>{item.category}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <div>
+    <h1>Women Products</h1>
+    <div>
+      <h2>Categories:</h2>
+      <ul>
+        <li>
+          <button onClick={() => setSelectedCategory('All')}>All</button>
+        </li>
+        {womenData.map((item) => (
+          <li key={item.id}>
+            <button onClick={() => setSelectedCategory(item.name)}>
+              {item.name}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+    <div>
+      <h2>Filter by: {selectedCategory}</h2>
+      {filteredData.map((item) => (
+        <div key={item.id}>
+          <h3>{item.title}</h3>
+          <p>Color: {item.color}</p>
+          <p>Size: {item.size}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 };
+
 
 export default Women;
