95d84dcef74755196d291a306e4505e0eaa08952
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-11-08 22:06:03 +0300

    getting images

---
+++ src/men.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import './men.css';
+
+function Men () {
+  const [accessories, setAccessories] = useState([]);
+  const [filteredAccessories, setFilteredAccessories] = useState([]);
+  const [selectedType, setSelectedType] = useState('All');
+
+  useEffect(() => {
+    // Fetch 
+    fetch('https://digaga-clads.onrender.com/men')
+      .then(response => response.json())
+      .then(data => {
+        setAccessories(data);
+        setFilteredAccessories(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  useEffect(() => {
+    if (selectedType === 'All') {
+      setFilteredAccessories(accessories);
+    } else {
+      const filtered = accessories.filter(all_men => all_men.type === selectedType);
+      setFilteredAccessories(filtered);
+    }
+  }, [selectedType, accessories]);
+
+  const handleFilterChange = (e) => {
+    setSelectedType(e.target.value);
+  }
+
+  const handleClickCard = (type) => {
+    setSelectedType(type);
+  }
+
+  return (
+    <div className='access'>
+      <h1 id='title'>Men</h1>
+      {/* <label htmlFor="typeFilter">Filter by Type:</label>
+      <select id="typeFilter" onChange={handleFilterChange} value={selectedType}>
+        <option value="All">All</option>
+        <option value="Necklace">Necklace</option>
+        <option value="Earrings">Earrings</option>
+        <option value="Bracelet">Bracelet</option>
+        <option value="Ring">Ring</option>
+      </select> */}
+      <div className="all_men-cards">
+        {accessories.map(all_men => (
+          <div
+            key={all_men.id}
+            className={`all_men-card ${all_men.type === selectedType ? 'active' : ''}`}
+            onClick={() => handleClickCard(all_men.type)}
+          >
+            <img src='{all_men.product_image}' />
+            <h2>{all_men.name}</h2>
+            <p>{all_men.description}</p>
+            <p>Price: ${all_men.unit_price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Men;
