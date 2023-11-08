99c44f4a419577ddae467ab091eba49178e8798a
Samolivyu <Samolivyumanki@outlook.com>
2023-11-08 11:31:30 +0300

    Working code, pending CSS & product filter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
-import React, { useState, useEffect} from 'react';
-import { BrowserRouter as Router, Route, useParams, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+import Women from './women';
 
 function App() {
-
-  const [Women, setWomen] = useState([]);
   const { category } = useParams();
 
-  useEffect(() => {
-    if (category) {
-      const apiUrl = 'digaga-clads-url/women/${category}';
-      
-      fetch(apiUrl)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setWomen(data);
-        })
-        .catch((error) => {
-          console.error('Error fetching Women data:', error);
-        });
-    }
-  }, [category]);
-
   return (
-    
     <Router>
       <Routes>
-      <Route path="/women/:category" 
-      element={<Women />} />
+        <Route
+          path="/women"
+          element={<Women />} 
+        />
       </Routes>
     </Router>
   );
