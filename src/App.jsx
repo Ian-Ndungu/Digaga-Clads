68ce09221549c4dd787c25e2f77f819e755ebcaa
Samolivyu <Samolivyumanki@outlook.com>
2023-11-09 16:38:53 +0300

    App commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import Women from './women';
+import "./App.css";
+import Women from "./women";
 
 function App() {
-  const { category } = useParams();
-
-  return (
-    <Router>
-      <Routes>
-        <Route
-          path="/women"
-          element={<Women />} 
-        />
-      </Routes>
-    </Router>
-  );
+  return <Women />;
 }
 
 export default App;
