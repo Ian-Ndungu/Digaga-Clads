f5b088046820181f9e981d77d4ec0431bd8d2684
Samolivyu <Samolivyumanki@outlook.com>
2023-11-09 16:31:50 +0300

    Implement the same content across branches.

--- src/men.jsx
+++ src/men.jsx
@@ -1,147 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import './men.css';
-import menData from './menData';
+import React, { useEffect, useState } from "react";
 
-const Men = () => 
-{
-  const { category } = useParams();
-  const [selectedCategory, setSelectedCategory] = useState('All');
-  const [selectedName, setSelectedName] = useState('All');
-  const [filteredData, setFilteredData] = useState([]);
-  const [men, setmen] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`http://localhost:5173/men/menData`);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const jsonData = await response.json();
-      setmen(jsonData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-      const jsonData = await response.json();
-      setFilteredData(jsonData);
-  };
-
+function Men() {
+  const [category, setCategory] = useState("men");
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetchData();
+    const init = async () => {
+      setLoading(true);
+
+      const url = `https://digaga-clads-main.onrender.com/${category}`;
+      // const url = `https://dummyjson.com/products/`;
+
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const json = await response.json();
+          setItems(json.products);
+
+          console.log(json);
+
+          setLoading(false);
+        } else {
+          setLoading(false);
+          throw response;
+        }
+      } catch (e) {
+        setItems(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    init();
   }, [category]);
 
-  const filterByColor = (color) => {
-    const filtered = men.filter((item) => item.details.some((detail) => detail.color === color));
-    setFilteredData(filtered);
-  };
-
-  const filterBySize = (size) => {
-    const filtered = men.filter((item) => item.details.some((detail) => detail.size === size));
-    setFilteredData(filtered);
-  };
-
-  const getUniqueName= (data) => {
-    const name= data.map((item) => item.name);
-    return [...new Set(name)];
-  };
-  //Populate by selected product
-
-  const getUniqueCategory= (data) => {
-    const category= data.map((item) => item.category);
-    return [...new Set(category)];
-  };
-  //populate by selected ctaegory
-
-    //to get data from the menData.
-
-
-  //-------- SETUP OF FUNCTIONS, DEPENDENCIES AND NECESSARY STATEMENTS ----------//------------//
-  let content = null;
-
-  if (selectedName === 'All') {
-    content = (
-      <div>
-        <h3>Clothing for {selectedName}</h3>
-        {filteredData.map((item) => (
-          <div key={item.id}>
-            <h4>{item.title}</h4>
-            <p>Color: {item.color}</p>
-            <p>Size: {item.size}</p>
-          </div>
-        ))}
+  function renderProduct(product) {
+    return (
+      <div key={product.id} className="product">
+        <a href="https://digaga-clads-main.onrender.com/men">
+          <h3>
+            {product.brand} - {product.title}
+          </h3>
+        </a>
+        <img
+          src={product.images[0]}
+          width={200}
+          height={200}
+          alt={product.name}
+        />
+        <p>{product.description}</p>
       </div>
     );
-  } else {
-    content = <div>null</div>;
   }
 
   return (
-    <div className='landing-page'>
-      <h1>men's Products</h1>
+    <>
+      <h1>Digaga Clads</h1>
       <div>
-      <h2>Filter by: {selectedName}</h2> 
-      <select
-      value={selectedName}
-      onChange={(e) => setSelectedName(e.target.value)}
-    >
-      <option value="All">All Categories</option>
-      {getUniqueName(menData).map((name) => (
-        <option key={name} value={name}>
-          {name}
-        </option>
-      ))}
-    </select>
-
-  {filteredData
-    .filter((item) => item.name === selectedName)
-    .map((item) => (
-      <div key={item.id}>
-        <h4>{item.title}</h4>
-        <p>Color: {item.color}</p>
-        <p>Size: {item.size}</p>
+        <select
+          name=""
+          value={category}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setCategory(e.target.value);
+          }}
+        >
+          <option value="men">Men</option>
+          <option value="women">Women</option>
+          <option value="children">Children</option>
+        </select>
       </div>
-    ))}
-
-      <input
-        type="text"
-        placeholder="Show products.."
-        value={filteredData}
-        onChange={(e) => filteredData(e.target.value)}
-      />
-
-      {filteredData
-        .filter(
-          (item) =>
-            item.name === selectedCategory &&
-            (filteredData === '' ||
-              item.title.toLowerCase().includes(filteredData.toLowerCase()))
-        )
-        .map((item) => (
-          <div key={item.id}>
-            <h4>{item.title}</h4>
-            <p>Color: {item.color}</p>
-            <p>Size: {item.size}</p>
-          </div>
-        ))}
-    </div>
-
-
-        <div className='landing-page'>
-          <h3>Filter by Product:</h3>
-          <select
-            value={selectedCategory}
-            onChange={(e) => setSelectedCategory(e.target.value)}
-          >
-            <option value="All">All</option>
-            {getUniqueCategory(menData).map((details) => (
-              <option key={details} value={details}>
-                {details}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
+      <div>{loading ? "Loading" : items.map(renderProduct)}</div>
+    </>
   );
-            }
+}
+
 export default Men;
