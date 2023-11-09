b5c80940822d529144c7458eb67c7f5e52e41c99
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-11-09 10:04:39 +0300

    merging with men1

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,20 @@ import './App.css';
 function App() {
   
   return (
-  <div>
-    < Homepage/>
-  </div>
+    <Router>
+    <div className='body'>
+      <Navbar /> {/* This component can remain outside the router */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/men" element={<Men/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  </Router>
   )
 }
 
